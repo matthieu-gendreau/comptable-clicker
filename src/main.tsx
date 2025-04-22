55f966e032f24038fe75c59c4078a8c83801480f
matthieu-gendreau <matthieu.g+github@hotmail.com>
2025-04-22 21:35:54 +0000

    feat: ajout de la fonction toggleDebug() pour activer/désactiver le mode debug depuis la console

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,28 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GameStateProvider } from './context'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Définir le type de window avec notre fonction toggleDebug
+declare global {
+  interface Window {
+    toggleDebug: () => void;
+  }
+}
+
+// Fonction pour activer/désactiver le mode debug
+window.toggleDebug = () => {
+  const gameState = JSON.parse(localStorage.getItem("gameState") || "{}");
+  gameState.debugMode = !gameState.debugMode;
+  localStorage.setItem("gameState", JSON.stringify(gameState));
+  location.reload();
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GameStateProvider>
+      <App />
+    </GameStateProvider>
+  </React.StrictMode>,
+)
